b1ed22f0129645a7baef98c08e9ac3117f996f05
jesse <jesse152@163.com>
2021-11-03 20:42:02 +0800

    feat: 注册微应用

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import "normalize.css/normalize.css";
 
 import dayjs from "dayjs";
 import ElementPlus, { ElLoading, ElMessage, ElMessageBox, ElNotification } from "element-plus";
+import { registerMicroApps, start } from "qiankun";
 import { createApp } from "vue";
 import { Store } from "vuex";
 
@@ -11,6 +12,7 @@ import TableColumn from "@/components/TableColumn/index.vue";
 
 import App from "./App.vue";
 import SvgIcon from "./components/SvgIcon/index.vue";
+import microApps from "./micro-app";
 import router from "./router";
 import store, { IState } from "./store";
 
@@ -49,3 +51,26 @@ app.component("ElTableColumn", TableColumn);
 app.use(router);
 app.use(store);
 app.mount("#app");
+
+registerMicroApps(microApps, {
+	beforeLoad: app => {
+		console.log("before load app.name====>>>>>", app.name);
+	},
+	beforeMount: [
+		app => {
+			console.log("[LifeCycle] before mount %c%s", "color: green;", app.name);
+		}
+	],
+	afterMount: [
+		app => {
+			console.log("[LifeCycle] after mount %c%s", "color: green;", app.name);
+		}
+	],
+	afterUnmount: [
+		app => {
+			console.log("[LifeCycle] after unmount %c%s", "color: green;", app.name);
+		}
+	]
+});
+
+start();
